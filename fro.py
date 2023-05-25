acb89328bd22107ebcb5c90cd234bcd7ba67ef41
SameerSunkada <sunkadasameer18@gmail.com>
2023-05-25 14:01:38 +0530

    Adding all files to remote repo

---
+++ fro.py
@@ -0,0 +1,55 @@
+# from flask import Flask, request, jsonify, render_template
+# from PIL import Image
+# import numpy as np
+# import tensorflow as tf
+
+# app = Flask(__name__)
+
+# @app.route("/code")
+# def code():
+#     return render_template('ai_deep.html')
+
+# @app.route("/predict", methods=["POST"])
+# def predict():
+#     model = tf.keras.models.load_model("model.h5")
+#     image = Image.open(request.files["image"])
+#     image = image.resize((48, 48))
+#     image = np.array(image) / 255.0
+#     image = np.expand_dims(image, axis=0)
+#     prediction = model.predict(image)
+#     emotion = np.argmax(prediction)
+#     print(emotion)
+#     return jsonify({"emotion": emotion})
+
+# if __name__ == "__main__":
+#     app.run(port = '4000', debug = True)
+
+
+from flask import Flask, request, jsonify, render_template
+from PIL import Image
+import numpy as np
+import tensorflow as tf
+
+app = Flask(__name__)
+
+@app.route("/code")
+def code():
+    return render_template('ai_deep.html')
+
+@app.route("/predict", methods=["POST"])
+def predict():
+    model = tf.keras.models.load_model("fer_model8.h5")
+    image = Image.open(request.files["image"])
+    image = image.convert('L')  # convert image to grayscale
+    image = image.resize((48, 48))
+    image = np.array(image) / 255.0
+    image = np.expand_dims(image, axis=0)
+    prediction = model.predict(image)
+    emotion = np.argmax(prediction)
+    x = ""
+    emotions = ('angry', 'disgust', 'fear', 'happy', 'sad', 'surprise', 'neutral')
+    x = emotions[int(str(emotion))]
+    return jsonify({"emotion": x})
+
+if __name__ == "__main__":
+    app.run(port='4000', debug=True)
